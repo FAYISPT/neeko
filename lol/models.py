9552f270aa4ef8a74bdf88fa5114075aeceafa49
FAYIS PT <fayispt@MUHAMMEDs-MacBook-Air.local>
2018-10-04 17:01:46 +0530

    new

--- lol/models.py
+++ lol/models.py
@@ -4,33 +4,33 @@ from django.db import models
 
 
 class Exam(models.Model):
-	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-	name = models.CharField(max_length=128)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=128)
 
-	class Meta:
+    class Meta:
         db_table = 'exams'
         verbose_name = _('exam')
         verbose_name_plural = _('exams')
         ordering = ('id',) 
 
     def __unicode__(self):
-    	return str(self.name)
+        return str(self.name)
 
 
 class ExamRegistrtion(models.Model):
-	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-	name = models.CharField(max_length=128)
-	mobile = models.CharField(max_length=128)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=128)
+    mobile = models.CharField(max_length=128)
     email = models.EmailField(blank=True,null=True)
-	register_number = models.CharField(max_length=128)
-	auto_id = models.PositiveIntegerField()
+    register_number = models.CharField(max_length=128)
+    auto_id = models.PositiveIntegerField()
     age = models.CharField(max_length=128)
 
     class Meta:
-    	db_table = 'exam_registration'
-    	verbose_name = _('registration')
-    	verbose_name_plural = _('registrations')
+        db_table = 'exam_registration'
+        verbose_name = _('registration')
+        verbose_name_plural = _('registrations')
 
     def __unicode__(self):
-    	return str(self.name)
-	
+        return str(self.name)
+    
