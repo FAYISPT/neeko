0a889c61c3c89ef4b123648d7b99ad8ae0a05b77
FAYIS PT <fayispt@MUHAMMEDs-MacBook-Air.local>
2018-10-04 17:00:31 +0530

    new

--- lol/models.py
+++ lol/models.py
@@ -1,6 +1,36 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Exam(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	name = models.CharField(max_length=128)
+
+	class Meta:
+        db_table = 'exams'
+        verbose_name = _('exam')
+        verbose_name_plural = _('exams')
+        ordering = ('id',) 
+
+    def __unicode__(self):
+    	return str(self.name)
+
+
+class ExamRegistrtion(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	name = models.CharField(max_length=128)
+	mobile = models.CharField(max_length=128)
+    email = models.EmailField(blank=True,null=True)
+	register_number = models.CharField(max_length=128)
+	auto_id = models.PositiveIntegerField()
+    age = models.CharField(max_length=128)
+
+    class Meta:
+    	db_table = 'exam_registration'
+    	verbose_name = _('registration')
+    	verbose_name_plural = _('registrations')
+
+    def __unicode__(self):
+    	return str(self.name)
+	
